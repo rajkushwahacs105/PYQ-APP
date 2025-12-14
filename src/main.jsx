53110207c9d178581ad7bc275dad4083fac7d810
rajkushwahacs105 <rajkushwahacs105@gmail.com>
2025-12-14 17:09:52 +0530

    fix gh-pages blank issue

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
-import { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
-import { HashRouter } from "react-router-dom";
+import './index.css'
+import { HashRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <HashRouter>
     <App />
   </HashRouter>
-);
+)
